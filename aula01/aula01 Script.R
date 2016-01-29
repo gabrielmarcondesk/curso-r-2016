45ae05b5160cc54709affdd154f4af81640f3e4f
adm <adm@labsoft.local>
2016-01-29 19:27:08 -0200

    up

---
+++ aula01/aula01 Script.R
@@ -0,0 +1,71 @@
+pi
+sin(pi / 2) + 2 * 3 - 4 ^ 2
+4%%3
+
+x <- 3
+y <- 10
+
+resto_de_x_div_y <- y%%x
+y%%x
+
+x == 4
+  
+# exemplo 'for'
+seq <- 1:10
+
+for (contador in seq) {
+    print(contador)
+}
+
+for(contador in seq) {
+    if (contador %% 2 == 0){
+      print("o número", contador, "é par")
+    } else {
+      print("o número", contador, "é impar")
+    }
+    print ("olá")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
